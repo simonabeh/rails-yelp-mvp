c0c8cf4dc4b7b5f791da4c972650178825b75e36
Simon Abehsera <abehsera.s@gmail.com>
2019-05-20 16:37:11 +0200

    Exo done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,18 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
   def index
     @reviews = Review.all
   end
 
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
@@ -22,5 +22,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
